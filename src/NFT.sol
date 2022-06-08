f7f7c6a9a47087bfee08c24aa59bfee76918bbda
CryptoDogg <d.enkikairo@gmail.com>
2022-06-08 17:20:39 +0900

    wip nft initialize

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,25 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-// import "openzeppelin-contracts/token/ERC721/ERC721.sol";
-// import "openzeppelin-contracts/access/Ownable.sol";
-import "openzeppelin-contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+// import "openzeppelin-contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+// import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "openzeppelin-contracts/utils/Counters.sol";
 import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract NFT is Initializable, OwnableUpgradeable, ERC721Upgradeable {
+contract NFT is Initializable, Ownable, ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    string private _name;
+    string private _symbol;
+
     function initialize(
-        address _owner,
-        string memory _name,
-        string memory _symbol
+        address owner,
+        string memory name,
+        string memory symbol
     ) public initializer {
-        __Ownable_init_unchained();
-        transferOwnership(_owner);
-        __ERC721_init_unchained(_name, _symbol);
+        transferOwnership(owner);
+        _name = name;
+        _symbol = symbol;
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
     }
 
     function mintNft(address receiver) external onlyOwner returns (uint256) {
